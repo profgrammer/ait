91797d009df4527292e935cb84157e944b9f5e2e
divyakhetan <divyakhetan1209@gmail.com>
2020-03-18 12:31:21 +0530

    added get all users, updated map

--- routes/adminHandler.js
+++ routes/adminHandler.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
+const fs = require('fs');
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -19,6 +21,20 @@ router.use(express.static('public'));
 router.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'dashboard.html'));
 });
+
+
+router.get('/getallusers', (req,res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'allusers.html'))
+});
+
+router.get('/users', (req, res) => {
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'users.json')));
+    console.log(users);
+    // const email_id = Object.keys(users);
+    // console.log(email_id);
+    res.status(200).send(users);
+
+})
 
 router.post('/addproduct', upload.fields([{name: 'photo', maxCount: 1}, {name: 'photos', maxCount: 10}]), (req, res) => {
     const {name, description, price} = req.body;
