139afb3e60be4238fb3493105a54c4b5b5a03b81
divyakhetan <divyakhetan1209@gmail.com>
2020-03-19 16:14:24 +0530

    added fb login

---
+++ configuration/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    /*
+     * This file contains the configurations information of Twitter login app.
+     * It consists of Twitter app information, database information.
+     */
+  
+    facebook_api_key: "226524102084072",
+    facebook_api_secret: "a22e9f8973864fd396bda013017fbf5b",
+    callback_url: "http://localhost:3000/facebookauth/callback",
+    use_database: false,
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: ""
+  };
+  
